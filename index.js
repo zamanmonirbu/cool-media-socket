d64f6e73ceb02bcda5b05c8099b369f2bafab3af
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-07-12 22:25:44 +0600

    update for deploy

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const io = require("socket.io")(8800, {
   },
 });
 
+// http://localhost:8800
+
 let activeUsers = [];
 
 io.on("connection", (socket) => {
@@ -30,10 +32,32 @@ io.on("connection", (socket) => {
   socket.on("send-message", (data) => {
     const { receiverId } = data;
     const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("Sending from socket to :", receiverId)
-    console.log("Data: ", data)
+    console.log("Sending from socket to :", receiverId);
+    console.log("Data: ", data);
     if (user) {
-      io.to(user.socketId).emit("recieve-message", data);
+      io.to(user.socketId).emit("receive-message", data);
+    }
+  });
+
+  // handle love reaction notification
+  socket.on("send-love-notification", (data) => {
+    const { receiverId } = data;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    console.log("Sending love notification to :", receiverId);
+    console.log("Data: ", data);
+    if (user) {
+      io.to(user.socketId).emit("receive-love-notification", data);
+    }
+  });
+
+  // handle comment notification
+  socket.on("send-comment-notification", (data) => {
+    const { receiverId } = data;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    console.log("Sending comment notification to :", receiverId);
+    console.log("Data: ", data);
+    if (user) {
+      io.to(user.socketId).emit("receive-comment-notification", data);
     }
   });
 });
