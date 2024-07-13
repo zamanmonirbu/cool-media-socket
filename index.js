05995c21274cc38d897056fac8cb32929c9a3583
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-07-13 17:01:04 +0600

    Final commit

--- index.js
+++ index.js
@@ -4,27 +4,21 @@ const io = require("socket.io")(8800, {
   },
 });
 
-// http://localhost:8800
 
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-  // add new User
   socket.on("new-user-add", (newUserId) => {
-    // if user is not added previously
     if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({ userId: newUserId, socketId: socket.id });
-      console.log("New User Connected", activeUsers);
+      // console.log("New User Connected", activeUsers);
     }
-    // send all active users to new user
     io.emit("get-users", activeUsers);
   });
 
   socket.on("disconnect", () => {
-    // remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
-    console.log("User Disconnected", activeUsers);
-    // send all active users to all users
+    // console.log("User Disconnected", activeUsers);
     io.emit("get-users", activeUsers);
   });
 
@@ -32,8 +26,6 @@ io.on("connection", (socket) => {
   socket.on("send-message", (data) => {
     const { receiverId } = data;
     const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("Sending from socket to :", receiverId);
-    console.log("Data: ", data);
     if (user) {
       io.to(user.socketId).emit("receive-message", data);
     }
@@ -43,8 +35,7 @@ io.on("connection", (socket) => {
   socket.on("send-love-notification", (data) => {
     const { receiverId } = data;
     const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("Sending love notification to :", receiverId);
-    console.log("Data: ", data);
+    // console.log("Sending love notification to :", receiverId);
     if (user) {
       io.to(user.socketId).emit("receive-love-notification", data);
     }
@@ -54,8 +45,7 @@ io.on("connection", (socket) => {
   socket.on("send-comment-notification", (data) => {
     const { receiverId } = data;
     const user = activeUsers.find((user) => user.userId === receiverId);
-    console.log("Sending comment notification to :", receiverId);
-    console.log("Data: ", data);
+    // console.log("Sending comment notification to :", receiverId);
     if (user) {
       io.to(user.socketId).emit("receive-comment-notification", data);
     }
